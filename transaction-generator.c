24467ec9ab3427f6c8ae4e857f3e1e4f5f80c29b
ImaJellyfis <joaotomasgfn@gmail.com>
2025-03-26 17:15:19 +0000

    Update transaction-generator.c

--- transaction-generator.c
+++ transaction-generator.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+//É preciso já criar uma transaction?
+int main(int argc, char *argv[]) {
+    int i, n_procs;
 
-int main() {
-    return 0;
+	if(argc!=3) {
+		printf("Wrong number of parameters\n");
+		exit(0);
+		}
+	if(atoi(argv[1])>=1 && atoi(argv[1])<=3 && atoi(argv[2])<=3000 && atoi(argv[2]) >=200){
+		printf("The parameters are correct\n");
+	}
+   
+	
+	return 0;
 }
