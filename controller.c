dd80b30d113728066c3123901cda7451ba911852
ImaJellyfis <joaotomasgfn@gmail.com>
2025-03-26 19:02:44 +0000

    Update controller.c

--- controller.c
+++ controller.c
@@ -6,34 +6,45 @@
 #include <signal.h>
 #define FPATH_CONFIG "config"
 #define MAX_THREADS 10
-int numero=0;
 //Só mutex? só podem entrar um de cada
 // Shared memory como é?
 // Realease está bem?
+int numero=0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexWrite = PTHREAD_MUTEX_INITIALIZER;
+sem_t empty;
 void handle_sigint(int sig) {
+	printf("Statistics:\n");
+	printf("Bitches:0\n");
+	printf("Genshin Impact female characters in your account: 58");
     printf("\nCleaning up...\n");
     exit(0);  // Exit gracefully
 }
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
 void* worker(void* i) {
     int w_id=*((int *)i);
     pthread_mutex_lock(&mutex);
     printf("Miner %d: numero=%d\n",w_id,numero);
     numero +=1;
-    pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
 }
+void* Validato(void* i){
+
+	printf("I sure do love to validate stuff \n");
+
+	pthread_exit(NULL);
+	
+    }
 
 
 int main() {
     signal(SIGINT, handle_sigint);
     int i, worker_id[MAX_THREADS];
     pthread_t my_thread[MAX_THREADS];
-
+	pthread_t validator;
     FILE *file;
     char buffer[255]; // Buffer to store each line
-
+	int trabalhos = MAX_THREADS;
     file = fopen(FPATH_CONFIG, "r");
     if (file == NULL) {
         perror("Could not open file.");
@@ -45,16 +56,18 @@ int main() {
     }
 
     fclose(file);
-
     for (i = 0; i < MAX_THREADS; i++) {
         worker_id[i]=i+1;
         pthread_create(&my_thread[i], NULL, worker, &worker_id[i]);
     }
+    pthread_create(&validator, NULL, Validato, &trabalhos);
 
     for(i=0; i< MAX_THREADS; i++){
         pthread_join(my_thread[i], NULL);
     }
-
+    pthread_join(validator, NULL);
+ 	while(1){
+ 	}
     printf("Done\n");
     exit(0);
     return 0;
