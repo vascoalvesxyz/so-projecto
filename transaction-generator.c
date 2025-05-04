dd23e1a3ca51417e93ca8b79d65a5311bd9c9867
ImaJellyfis <joaotomasgfn@gmail.com>
2025-05-04 22:16:37 +0100

    Update transaction-generator.c

--- transaction-generator.c
+++ transaction-generator.c
@@ -1,6 +1,10 @@
+//Vasco Alves 2022228207 Joao Neto 2023234004
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SEM_POOL_EMPTY "/dei_pool_empty"
+#define SEM_POOL_FULL "/dei_pool_full"
+#define SEM_POOL_MUTEX "/dei_pool_mutex"
+//WRITE TRANSACTIONS
 #include "transaction.h"
 
 Transaction transcation_generate() {
@@ -9,11 +13,20 @@ Transaction transcation_generate() {
 }
 
 int main(int argc, char *argv[]) {
-
-    if(argc!=3) {
+	// Open semaphores
+	if(argc!=3) {
         printf("Wrong number of parameters\n");
         exit(0);
     }
+    sem_t *sem_empty = sem_open(SEM_POOL_EMPTY, 0);
+    sem_t *sem_full = sem_open(SEM_POOL_FULL, 0);
+    sem_t *sem_mutex = sem_open(SEM_POOL_MUTEX, 0);
+    if (sem_empty == SEM_FAILED || sem_full == SEM_FAILED || sem_mutex == SEM_FAILED) {
+        printf("Failed to open semaphores. Is controller running?\n");
+        exit(EXIT_FAILURE);
+    }
+    //ADICIONAR LOGICA DE CONTINUO TRANSACTION
+    
 
     int reward = atoi(argv[1]);
     int time_ms = atoi(argv[2]);
@@ -21,7 +34,9 @@ int main(int argc, char *argv[]) {
     if(reward >=1 && reward <=3 && time_ms <=3000 && time_ms >=200){
         printf("The parameters are correct.\n");
     }
-
+	sem_close(sem_empty);
+    sem_close(sem_full);
+    sem_close(sem_mutex);
 
     return 0;
 }
